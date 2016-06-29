75e6991399dc94250cfe9e223e1f0b4651ac8e31
Toby Marks <tobyjmarks@gmail.com>
2016-06-29 16:16:44 -0500

    Delete pre.sql

--- pre.sql
+++
@@ -1,9 +0,0 @@
-begin
-
-    for i in (select * from dual where not exists (select * from all_users where username = 'LIBRARY')) loop
-        execute immediate 'create user library identified by library';
-        execute immediate 'grant connect, resource, unlimited tablespace to library';
-    end loop;
-    
-end;
-/
